029e1a2f85c45d7d7da2cbc5d58561432b36d233
mousazadeh <m.mousazadeh1998@gmail.com>
2025-08-18 12:11:28 +0330

    add cors files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
+            'cors' => \App\Http\Middleware\Cors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
