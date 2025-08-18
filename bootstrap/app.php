84d746c30f3ecf4164ab2c95008bbf7e7c101e08
mousazadeh <m.mousazadeh1998@gmail.com>
2025-08-18 12:31:08 +0330

    edite config cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
-            'cors' => \App\Http\Middleware\Cors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
