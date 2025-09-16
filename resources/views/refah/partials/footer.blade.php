fda8ee4f056370be00bd7c1e77958ec31f56be07
rezaee <rezaee>
2025-09-16 10:57:56 +0430

    add enamad for refah

--- resources/views/refah/partials/footer.blade.php
+++ resources/views/refah/partials/footer.blade.php
@@ -5,6 +5,9 @@
         <div class="footer-phone">تلفن: <a href="tel:02126206918" style="color: #e2e8f0; text-decoration: none;">26206918-021</a></div>
         <div class="footer-phone">تلفن: <a href="tel:02126206725" style="color: #e2e8f0; text-decoration: none;">26206725-021</a></div>
         <div class="footer-address">آدرس: تهران،الهیه خ بیدار برج جم پ۴۲</div>
+        <div class="footer-enamad">
+            <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=647864&Code=zo0HPX2dZRTijQjyL604AiuXyLSlUcR0'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=647864&Code=zo0HPX2dZRTijQjyL604AiuXyLSlUcR0' alt='' style='cursor:pointer' code='zo0HPX2dZRTijQjyL604AiuXyLSlUcR0'></a>
+        </div>
         <div class="footer-bottom">
         کلیه حقوق مادی و معنوی این سایت متعلق به سامانه رفاه کالا می باشد.
         </div>
